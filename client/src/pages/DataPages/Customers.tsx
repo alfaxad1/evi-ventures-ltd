0108635135dd5bc7e83059f5cbd972c703248c79
alfaxad1 <alfaxadder@gmail.com>
2025-04-11 00:57:13 +0300

    Add react-toastify for notifications and enhance CustomersForm with form handling and validation

--- client/src/pages/DataPages/Customers.tsx
+++ client/src/pages/DataPages/Customers.tsx
@@ -1,19 +1,105 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "../../../src/components/ui/table";
+
+interface Customer {
+  id: number;
+  first_name: string;
+  phone: string;
+  national_id: string;
+  address: string;
+  occupation: string;
+}
 
 const Customers = () => {
   useEffect(() => {
     fetchData();
   });
+
+  const [customerData, setCustomerData] = useState<Customer[]>([]);
   const fetchData = async () => {
     try {
       const response = await axios.get("http://localhost:8000/api/customers");
       console.log("Data fetched successfully:", response.data);
+      setCustomerData(response.data.data);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
-  return <div className="">Customers</div>;
+  return (
+    <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
+      <div className="max-w-full overflow-x-auto">
+        <div className="min-w-[1102px]">
+          <Table>
+            {/* Table Header */}
+            <TableHeader className="border-b border-gray-100 dark:border-white/[0.05]">
+              <TableRow>
+                <TableCell
+                  isHeader
+                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                >
+                  Name
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                >
+                  Phone Number
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                >
+                  ID Number
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                >
+                  Address
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                >
+                  Occupation
+                </TableCell>
+              </TableRow>
+            </TableHeader>
+
+            {/* Table Body */}
+            <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05]">
+              {customerData.map((customer) => (
+                <TableRow key={customer.id}>
+                  <TableCell className="px-5 py-4 sm:px-6 text-start">
+                    {customer.first_name}
+                  </TableCell>
+                  <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                    {customer.phone}
+                  </TableCell>
+                  <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                    {customer.national_id}
+                  </TableCell>
+                  <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                    {customer.address}
+                  </TableCell>
+                  <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+                    {customer.occupation}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Customers;
--- client/src/pages/DataPages/CustomersForm.tsx
+++ client/src/pages/DataPages/CustomersForm.tsx
@@ -1,39 +1,196 @@
+import { Calendar1Icon } from "lucide-react";
 import Input from "../../components/form/input/InputField";
 import Label from "../../components/form/Label";
-import Select from "../../components/form/Select";
+import Flatpickr from "react-flatpickr";
+import { useState } from "react";
+import Button from "../../components/ui/button/Button";
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+
+interface CustomerData {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  nationalId: string;
+  DOB?: string;
+  gender: string;
+  address: string;
+  county: string;
+  occupation: string;
+  monthlyIncome: string;
+  createdBy?: number;
+}
 
 const CustomersForm = () => {
+  const [formData, setFormData] = useState<CustomerData>({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    nationalId: "",
+    DOB: "",
+    gender: "",
+    address: "",
+    county: "",
+    occupation: "",
+    monthlyIncome: "",
+    createdBy: 1,
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleDateChange = (selectedDates: Date[]) => {
+    if (selectedDates.length > 0) {
+      const formattedDate = selectedDates[0].toISOString().split("T")[0];
+      setFormData({
+        ...formData,
+        DOB: formattedDate,
+      });
+    }
+  };
+  const resetForm = () => {
+    setFormData({
+      firstName: "",
+      lastName: "",
+      phone: "",
+      nationalId: "",
+      DOB: "",
+      gender: "",
+      address: "",
+      county: "",
+      occupation: "",
+      monthlyIncome: "",
+    });
+  };
+  const save = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/customers",
+        formData
+      );
+      resetForm();
+      toast.success(response.data.message);
+      console.log(response.data.message);
+      resetForm();
+    } catch (err) {
+      console.error("Error saving", err);
+    }
+  };
+
   return (
-    <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
-      <div className="space-y-6">
-        <div>
-          <Label htmlFor="name">Name</Label>
-          <Input type="text" placeholder="name" />
+    <>
+      <ToastContainer />
+      <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
+        <div className="space-y-6">
+          <div>
+            <Label htmlFor="first-name">First Name</Label>
+            <Input
+              type="text"
+              placeholder="first name"
+              name="firstName"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div>
+            <Label htmlFor="last-name">Last Name</Label>
+            <Input
+              type="text"
+              placeholder="last name"
+              name="lastName"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div>
+            <Label htmlFor="phone">Phone Number</Label>
+            <Input
+              type="text"
+              placeholder="phone number"
+              name="phone"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div>
+            <Label htmlFor="ID">ID Number</Label>
+            <Input
+              type="number"
+              placeholder="ID"
+              name="nationalId"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div>
+            <Label htmlFor="DOB">Date of Birth</Label>
+            <div className="relative w-full flatpickr-wrapper">
+              <Flatpickr
+                name="DOB"
+                options={{
+                  dateFormat: "Y-m-d",
+                }}
+                placeholder="Select date of birth"
+                onChange={(selectedDates) => handleDateChange(selectedDates)}
+                className="h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3  dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30  bg-transparent text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-brand-500/20 dark:border-gray-700  dark:focus:border-brand-800"
+              />
+              <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
+                <Calendar1Icon className="size-6" />
+              </span>
+            </div>
+          </div>
         </div>
-        <div>
-          <Label htmlFor="name">Name</Label>
-          <Input type="text" placeholder="name" />
+        <div className="space-y-6">
+          <div>
+            <Label htmlFor="gender">Gender</Label>
+            <Input
+              type="text"
+              placeholder="gender"
+              name="gender"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div>
+            <Label htmlFor="address">Address</Label>
+            <Input
+              type="text"
+              placeholder="address"
+              name="address"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div>
+            <Label htmlFor="county">County</Label>
+            <Input
+              type="text"
+              placeholder="county"
+              name="county"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div>
+            <Label htmlFor="occupation">Occupation</Label>
+            <Input
+              type="text"
+              placeholder="occupation"
+              name="occupation"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div>
+            <Label htmlFor="income">Monthly Income</Label>
+            <Input
+              name="monthlyIncome"
+              type="number"
+              placeholder="monthlyIncome"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
         </div>
+        <Button className="w-full mt-6" onClick={save}>
+          Submit
+        </Button>
       </div>
-      <div className="space-y-6">
-        <div>
-          <Label htmlFor="name">Name</Label>
-          <Input type="text" placeholder="name" />
-        </div>
-        <div>
-          <Label htmlFor="name">Name</Label>
-          <Select
-            options={[
-              { value: "1", label: "Option 1" },
-              { value: "2", label: "Option 2" },
-              { value: "3", label: "Option 3" },
-            ]}
-            placeholder="Select Option"
-            onChange={(value) => console.log(value)}
-          />
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
